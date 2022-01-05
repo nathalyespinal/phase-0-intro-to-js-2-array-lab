53d5e0a45e055f5eada2b0a9867bc072e1997fe1
NathalyEspinal <espinalnathaly@gmail.com>
2022-01-04 21:03:27 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
-// Write your solution here!
+
+   
+      const cats = ["Milo", "Otis", "Garfield"];
+
+      
+     function destructivelyAppendCat(name) {
+      return cats.push("Ralph");
+     }
+  
+
+     function destructivelyPrependCat(name) {
+      return cats.unshift("Bob");
+     }
+   
+     function destructivelyRemoveLastCat(name) {
+      return cats.pop("Ralph");
+     }
+
+     function destructivelyRemoveFirstCat(name) {
+      return cats.shift("Bob");
+     }
+     
+     function appendCat(name) {
+      return [...cats, name]
+     }
+
+      function prependCat (name) {
+       return [name, ...cats,] 
+     }
+
+         
+     function removeLastCat(name) {
+            return cats.slice(0, cats.length - 1);
+     }
+
+     function removeFirstCat(name) {
+           return cats.slice(1);
+     }
+
+   
+
+
+
+   
+
+  
